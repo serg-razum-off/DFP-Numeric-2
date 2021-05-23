ea182bda2615fc17e00478936c6d4ccd2803b085
SergiyR <sergiy.razumov@gmail.com>
2021-05-23 14:08:22 +0300

    SR: all datafiles merged into DataManager.data_btc

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # DFP-Numeric-2
 
-Project name: Bitcoin Price Prediction
+<u>**Project name**</u>: Bitcoin Price Prediction
 
-Data sources:
+<u>**GitHub**</u>: https://github.com/Serg-Protsenko/DFP-Numeric-2
 
-https://finance.yahoo.com/quote/BTC-USD/history/?guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAI-4t433iLXfYDEOypaT4ASxQ7llofVisZle1NVQSHaMzqOgRqx_ZTUqJwGk0OcUGYpmLygU3jJLesPIakedgWcyrQaOznwFwjbsLnCrYBMexRYd-WE-QsaQ516eW24zsapaH4SYQtj5YY6KP2Vg8_h7LpWpQBx6m9LKrZZeyskt&guccounter=2
+<u>**Data sources**</u>: <br>
+* Bitcoin Exchange Rates Statistics https://www.investing.com/crypto/bitcoin/historical-data <br>
+* Bitcoin Average Block Size https://www.quandl.com/data/BCHAIN/AVBLS-Bitcoin-Average-Block-Size <br>
+* Bitcoin Hash Rate https://www.quandl.com/data/BCHAIN/HRATE-Bitcoin-Hash-Rate <br>
+* Bitcoin Miners Revenue https://www.quandl.com/data/BCHAIN/MIREV-Bitcoin-Miners-Revenue <br>
+* Bitcoin My Wallet Number of Transaction Per Day https://www.quandl.com/data/BCHAIN/MWNTD-Bitcoin-My-Wallet-Number-of-Transaction-Per-Day <br>
+* Bitcoin Cost Per Transaction https://www.quandl.com/data/BCHAIN/CPTRA-Bitcoin-Cost-Per-Transaction
+
 
 
--- classes/cls_data.py
+++ classes/cls_data.py
@@ -3,22 +3,26 @@ import os
 import gc
 
 
-class NumData:
+class DataManager:
     """
     SR: reads the num files related to trades of bitcoin
     """
 
-    def __init__(self, dir_path, btc_exch_rates_filename, btc_total_circ_filename):
+    def __init__(self, dir_path: str, btc_exch_rates_filename: str):
         """
-        inits the data class
+        inits the DataManager class
+
         :param dir_path: path to csvs
         :param btc_exch_rates_filename: name of file with btc_quotes
-        :param btc_total_circ_filename: name of file with btc in circulation
+        :param btc_other_filenames: list of filenames that store additional data
+                                    All have to be in same format: dateCol, ValCol
         """
-        self._data_btc = None
-        self.clean_btc_exch_rates_data(dir_path, btc_exch_rates_filename)
+        self.data_btc = None
+        self._btc_exch_rates_filename = btc_exch_rates_filename
+        self._set_self_data_btc(dir_path, btc_exch_rates_filename)
+        self._self_data_btc_Add_other_metrics(dir_path=dir_path)
 
-    def clean_btc_exch_rates_data(self, dir_path, file_name):
+    def _set_self_data_btc(self, dir_path, file_name):
         # reading exch_rate csv
         btc_exch_raw = (
             pd.read_csv(os.path.join(dir_path, file_name))
@@ -52,13 +56,30 @@ class NumData:
         btc_exch_raw['Growth'] = btc_exch_raw['Growth'].str.replace('%', '').astype(
             float) / 100
 
-        self._data_btc = btc_exch_raw
+        self.data_btc = btc_exch_raw.set_index("Date")
         del btc_exch_raw
         gc.collect()
 
-    def merge_btc_exch_rates_w_btc_in_circulation(self, dir_path, file_name):
-        btc_in_circ_raw = pd.read_csv(os.path.join(dir_path, file_name))
-        btc_in_circ_raw['Data'] = pd.to_datetime(btc_in_circ_raw['Date'])
-        btc_in_circ_raw['Value'] = btc_in_circ_raw['Value'].astype(float)
-        btc_in_circ_raw['Btc_Mined'] = -btc_in_circ_raw['Value'].diff()
-        
+    def _self_data_btc_Add_other_metrics(self, dir_path: str):
+        """
+        Gets CSV files (columns: date, value), merges them, adds to Exchange Rate
+
+        :param dir_path: path to csv files
+        :param file_list: list of file_names that have to be extracted and added to exch rate
+        """
+        result = None
+        file_list = [f for f in os.listdir(dir_path)
+                     if (self._btc_exch_rates_filename not in f)
+                     and ("propert" not in f)]
+
+        for file_name in file_list:
+            curr_file = pd.read_csv(os.path.join(dir_path, file_name))
+            curr_file['Date'] = pd.to_datetime(curr_file['Date'])
+            curr_file['Value'] = curr_file['Value'].astype(float)
+            if 'TOTBC' in file_name:
+                curr_file['Btc_Mined'] = -curr_file['Value'].diff()
+                curr_file['Btc_Mined'].fillna(method='bfill', inplace=True)  # filling last day (Nan) as one before
+            curr_file.set_index('Date', inplace=True)
+            curr_file.rename(columns={"Value": file_name.split('-')[-1].split(".")[0]}, inplace=True) # rename Value col
+
+            self.data_btc = self.data_btc.merge(curr_file, right_index=True, left_index=True)
