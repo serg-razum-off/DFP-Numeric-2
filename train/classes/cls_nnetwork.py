65f2e2c01d70cd9320db15a13741f3cea497c50f
SergiyR <sergiy.razumov@gmail.com>
2021-06-05 18:11:10 +0300

    SR: example from MLMastery LSTM article /started/

--- train/classes/cls_nnetwork.py
+++ train/classes/cls_nnetwork.py
@@ -1,3 +1,9 @@
+# Based on 
+# https://machinelearningmastery.com/how-to-develop-lstm-models-for-time-series-forecasting
+# https://towardsdatascience.com/the-time-series-transformer-2a521a0efad3
+# https://machinelearningmastery.com/timedistributed-layer-for-long-short-term-memory-networks-in-python/
+# https://machinelearningmastery.com/how-to-develop-lstm-models-for-multi-step-time-series-forecasting-of-household-power-consumption/
+
 from tensorflow import keras
 
 
